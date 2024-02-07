4da312ce53f45083c22e1e585c4207bbdb04cdba
Dinesh Kumaar Rajendran <48643376+dineshkumaar-rajendran@users.noreply.github.com>
2024-02-07 23:05:32 +0530

    Update variables.py

--- days/code/variables.py
+++ days/code/variables.py
@@ -1,8 +1,17 @@
+x = 4  # global variable
+
+
 def function_v():
     y = "kumaar"  # local variable
     print(y)
     print(x)
 
 
-x = "Dinesh"  # global variable
+def function_two():
+    global x # global keyword 
+    x += 5
+    print(x)
+
+
 function_v()
+function_two()
